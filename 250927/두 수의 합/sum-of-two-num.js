2707c4955bbef7b8d05f28cd63a2930ac9d566fc
Sunghee <70868454+sungheeyun-bit@users.noreply.github.com>
2025-09-27 23:54:50 +0900

    Title: 두 수의 합, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250927/두 수의 합/sum-of-two-num.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+
+const sum = new Map();
+
+
+for(let i=0; i<n; i++) {
+    for(let j=0; j<n; j++) {
+        if(sum.has(arr[i] + arr[j])) {
+            sum.set(arr[i] + arr[j], [...sum.get(arr[i] + arr[j]), [arr[i], arr[j]]]);
+        } else {
+            sum.set(arr[i] + arr[j], [[arr[i], arr[j]]]);
+        }
+    }
+}
+
+
+
+if(sum.has(k)) {
+    let target = sum.get(k).flat();
+    let unique = new Set(target);
+    
+    console.log(Math.ceil(unique.size / 2));
+}
