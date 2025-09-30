005195e2e7be37c1ba286f85ab7ad521262eb388
Sunghee <70868454+sungheeyun-bit@users.noreply.github.com>
2025-09-30 15:24:56 +0900

    Title: 두 수의 합, Time: 1214ms, Memory: 160MB, Status: Memory Limit Exceed - Codetree

---
+++ 250930/두 수의 합/sum-of-two-num.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+const sumMap = new Map();
+arr.sort();
+
+const combi = [];
+for(let i=0; i<n; i++) {
+    for(let j=i+1; j<n; j++) {
+        let sum = arr[i] + arr[j];
+        sumMap.set(sum, sumMap.get(sum) + 1 || 1);
+    }
+}
+
+console.log(sumMap.get(k));
+
