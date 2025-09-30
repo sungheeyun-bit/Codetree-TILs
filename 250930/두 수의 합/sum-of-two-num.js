c89d28c112acf93befa88f1723c87348993d73e5
Sunghee <70868454+sungheeyun-bit@users.noreply.github.com>
2025-09-30 17:11:02 +0900

    Title: 두 수의 합, Time: 121ms, Memory: 25MB, Status: Passed - Codetree

--- 250930/두 수의 합/sum-of-two-num.js
+++ 250930/두 수의 합/sum-of-two-num.js
@@ -4,21 +4,22 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 
+// Please Write your code here.
 const freq = new Map();
-for (const v of arr) {
-  freq.set(v, (freq.get(v) || 0) + 1);   
+let count = 0;
+for(const v of arr) {
+  freq.set(v, (freq.get(v) || 0) + 1);
 }
 
-let count = 0;
-for (const [v, c] of freq.entries()) {
-  const u = k - v;
-  if (!freq.has(u)) continue;
+for(const [v, c] of freq.entries()) {
+  let u = k - v;
+  if(!freq.has(u)) continue;
 
-  if (v === u) {
+  if(u === v) {
     count += c * (c - 1) / 2;
   } else if (v < u) {
     count += c * freq.get(u);
   }
 }
 
-console.log(count);
+console.log(count)
