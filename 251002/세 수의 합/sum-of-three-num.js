f72f4e2f1b4bdfe79cd1eeb44c6b9b2a9a883484
Sunghee <70868454+sungheeyun-bit@users.noreply.github.com>
2025-10-02 18:50:48 +0900

    Title: 세 수의 합, Time: 76ms, Memory: 10MB, Status: Passed - Codetree

--- 251002/세 수의 합/sum-of-three-num.js
+++ 251002/세 수의 합/sum-of-three-num.js
@@ -6,16 +6,32 @@ const arr = input[1].split(' ').map(Number);
 
 // Please Write your code here.
 
-let count = 0;
+// let count = 0;
 
-for (let i = 0; i < n - 2; i++) {
-  for (let j = i + 1; j < n - 1; j++) {
-    for (let l = j + 1; l < n; l++) {
-      if (arr[i] + arr[j] + arr[l] === k) {
-        count++;
-      }
+// for(let i=0; i<n-2; i++) {
+//     for(let j=i+1; j<n-1; j++) {
+//         for(let l=j+1; l<n; l++) {
+//             let sum = arr[i] + arr[j] + arr[l];
+//             if(sum === k) count++;
+//         }
+//     }
+// }
+
+// console.log(count);
+
+const freq = new Map();
+let result = 0;
+
+for(let num of arr) freq.set(num, (freq.get(num) || 0) + 1);
+
+for(let i=0; i<n; i++) {
+    
+    freq.set(arr[i], freq.get(arr[i]) - 1);
+    for(let j=0; j<i; j++) {
+        let target = k - arr[i] - arr[j];
+        
+        result+=(freq.get(target) ?? 0)
     }
-  }
 }
 
-console.log(count);
+console.log(result);
