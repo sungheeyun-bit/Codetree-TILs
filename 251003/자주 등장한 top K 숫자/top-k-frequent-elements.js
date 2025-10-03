33b6c26faf6b08b774850e8b155ec49eb7c0b053
Sunghee <70868454+sungheeyun-bit@users.noreply.github.com>
2025-10-03 18:04:37 +0900

    Title: 자주 등장한 top K 숫자, Time: 172ms, Memory: 32MB, Status: Passed - Codetree

--- 251003/자주 등장한 top K 숫자/top-k-frequent-elements.js
+++ 251003/자주 등장한 top K 숫자/top-k-frequent-elements.js
@@ -4,24 +4,24 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const arr = input[1].trim().split(' ').map(Number);
 
-// Please Write your code here.
-
+// 1. 각 숫자의 등장 횟수를 Map에 저장
 const count = new Map();
-
 arr.forEach(num => {
     count.set(num, (count.get(num) || 0) + 1);
-})
-
-
-const countArr = [...count];
-const sort = countArr.sort((a, b) => {
-    if(b[1] === a[1]) return b[0] - a[0];
-    else return b[1] - a[1];
 });
 
-let result = '';
-for(let i=0; i<k; i++) {
-    result += `${sort[i][0]} `
-}
+// 2. [숫자, 횟수] 배열로 변환
+const countArr = [...count];
+
+// 3. 정렬: 
+// - 횟수가 큰 순서
+// - 횟수가 같으면 숫자가 큰 순서
+countArr.sort((a, b) => {
+    if (b[1] === a[1]) return b[0] - a[0];
+    return b[1] - a[1];
+});
+
+// 4. 상위 k개의 숫자만 추출
+const result = countArr.slice(0, k).map(item => item[0]).join(" ");
 
 console.log(result);
